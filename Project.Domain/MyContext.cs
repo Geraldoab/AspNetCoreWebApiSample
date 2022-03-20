e4a105a40ed652cd8ff38760b1d127666eb450fd
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2022-03-20 20:49:08 -0300

    Update MyContext.cs

--- Project.Domain/MyContext.cs
+++ Project.Domain/MyContext.cs
@@ -7,6 +7,6 @@ public class MyContext: DbContext
     public DbSet<Employee> Employees { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options) {
-        options.UseSqlServer(@"Server=DESKTOP-38O7D87\SQLEXPRESS;Database=MyCompany;Trusted_Connection=True;");
+        options.UseSqlServer(@"{ your database connection string }");
     }
 }
